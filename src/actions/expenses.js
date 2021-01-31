9ea1362175b6d1418ecba806287eb8a310439642
mxie <836728902@qq.com>
2021-01-31 21:38:30 +0200

    update api in project

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -27,6 +27,14 @@ export const editExpense = (id, updates) => ({
     updates
 });
 
+export const startEditExpense = (id, updates) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).update(updates).then(() => {
+            dispatch(editExpense(id, updates));
+        })
+    }
+};
+
 export const startAddExpense = (expenseData = {}) => {
     return (dispatch) => {
         const {
