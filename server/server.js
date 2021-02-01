86aa337ea639be65e55b5741874297ece2943dda
mxie <836728902@qq.com>
2021-02-01 04:04:09 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -5,11 +5,11 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
  
-var host = process.env.HOST || '0.0.0.0';
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
+// var host = process.env.HOST || '0.0.0.0';
+// // Listen on a specific port via the PORT environment variable
+// var port = process.env.PORT || 8080;
  
-var cors_proxy = require('cors-anywhere');
+// var cors_proxy = require('cors-anywhere');
  
 
 app.use(express.static(publicPath));
