690dfaa6acd905a671eb7c983ec5143ee0d737e9
mxie <836728902@qq.com>
2021-02-01 13:45:54 +0200

    cor fix

--- src/actions/repositories.js
+++ src/actions/repositories.js
@@ -14,7 +14,7 @@ export const fetchRepository = (userName, pageIndex) => {
     return (dispatch) => {
        
         const repositoryAPI =
-            `https://cors-anywhere.herokuapp.com/https://git-info-backend.herokuapp.com/users/${userName}/repos?page=${pageIndex}&size=1`; // page need change
+            `https://git-info-backend.herokuapp.com/users/${userName}/repos?page=${pageIndex}&size=1`; // page need change
         return axios.get(repositoryAPI).then((res) => {
             const repositories = res.data;
             dispatch(setRepositories(repositories));
--- src/actions/user.js
+++ src/actions/user.js
@@ -8,7 +8,7 @@ export const setUser = (user) => ({
 export const fetchUser = (userName) => {
     return (dispatch) => {
         //const userAPI = `https://git-info-backend.herokuapp.com/users/${userName}`;
-        const userAPI = `https://cors-anywhere.herokuapp.com/https://git-info-backend.herokuapp.com/users/${userName}`
+        const userAPI = `https://git-info-backend.herokuapp.com/users/${userName}`
         return axios.get(userAPI ).then((res) => {
             const userInfo = res.data;
             dispatch(setUser(userInfo));
