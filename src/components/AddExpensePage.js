bfff104c993fa40e6b0d08146b6c2b9631c2cf62
mxie <836728902@qq.com>
2021-02-12 23:06:04 +0200

    add style

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -5,11 +5,17 @@ import { addExpense, startAddExpense } from '../actions/expenses';
 
 const AddExpensePage = (props) => (
     <div>
-        <h1>add expense</h1>
-        <ExpenseForm onSubmit={(expense) => {
-            props.dispatch(startAddExpense(expense));
-            props.history.push('/');
-        }} />
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Add Expense</h1>
+            </div>
+        </div>
+        <div className="content-container">
+            <ExpenseForm onSubmit={(expense) => {
+                props.dispatch(startAddExpense(expense));
+                props.history.push('/');
+            }} />
+        </div>
     </div>
 );
 const mapDispatchToProps = (dispatch) => ({
--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -6,17 +6,26 @@ import { startEditExpense, startRemoveExpense } from '../actions/expenses';
 const EditExpensePage = (props) => {
     return (
         <div>
-            <ExpenseForm
-                expense={props.expense}
-                onSubmit={(expense) => {
-                    props.dispatch(startEditExpense(props.expense.id, expense));
-                    props.history.push('/');
-                }}
-            />
-            <button onClick={() => {
-                props.dispatch(startRemoveExpense({ id: props.expense.id }));
-                props.history.push('/');
-            }}>Remove</button>
+            <div className="page-header">
+                <div className="content-container">
+                    <h1 className="page-header__title">Edit Expense</h1>
+                </div>
+            </div>
+            <div className="content-container">
+                <ExpenseForm
+                    expense={props.expense}
+                    onSubmit={(expense) => {
+                        props.dispatch(startEditExpense(props.expense.id, expense));
+                        props.history.push('/');
+                    }}
+                />
+                <button
+                    className="button button--secondary"
+                    onClick={() => {
+                        props.dispatch(startRemoveExpense({ id: props.expense.id }));
+                        props.history.push('/');
+                    }}>Remove Expense</button>
+            </div>
         </div>
     )
 }
