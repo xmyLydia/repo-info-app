2a6d9032c6f9e77a970106116de48fa9bcc6ac07
mxie <836728902@qq.com>
2021-02-13 14:07:24 +0200

    style

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import 'normalize.css/normalize.css';
 import './style/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
 let hasRenderd = false;
@@ -25,7 +26,7 @@ const jsx = (
         <AppRouter />
     </Provider>
 )
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
