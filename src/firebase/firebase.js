6a152b3377f8090616265f338073d75b232bf125
mxie <836728902@qq.com>
2021-02-27 22:04:31 +0200

    structure change

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,6 @@
 import * as firebase from 'firebase';
+import firebaseConfig from './firebaseConfig';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAN3UZJYNPz4HNdGj9aMgZd1jgh93ORrSk",
-    authDomain: "git-info-app.firebaseapp.com",
-    databaseURL: "https://git-info-app-default-rtdb.firebaseio.com",
-    projectId: "git-info-app",
-    storageBucket: "git-info-app.appspot.com",
-    messagingSenderId: "450106870391",
-    appId: "1:450106870391:web:25c24caf53d5ae6be75472"
-};
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 const googleAuthProvider = new firebase.auth.OAuthProvider('google.com');
